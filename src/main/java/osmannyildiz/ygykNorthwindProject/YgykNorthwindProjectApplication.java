0a127dce553ee7c42cdee3831af34d4c755229a7
Osman Nuri Yıldız <iamosmannyildiz@gmail.com>
2021-12-31 17:44:40 +0300

    Initial commit

---
+++ src/main/java/osmannyildiz/ygykNorthwindProject/YgykNorthwindProjectApplication.java
@@ -0,0 +1,13 @@
+package osmannyildiz.ygykNorthwindProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YgykNorthwindProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YgykNorthwindProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/osmannyildiz/ygykNorthwindProject/YgykNorthwindProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package osmannyildiz.ygykNorthwindProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YgykNorthwindProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
